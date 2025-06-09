a02d2445b3eb4ef6a5a72275af03c6a8a5c1c6c6
Iftekharul Abedeen <44981422+Abedeen25@users.noreply.github.com>
2025-06-09 21:10:21 +0600

    sidebar complete

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,8 +3,22 @@ import { Link } from "react-router-dom";
 import "../index.scss";
 
 function Sidebar() {
+  const [calenderVisible, setCalenderVisible] = useState(false);
+  const [sidebarVisible, setSidebarVisible] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+    console.log(sidebarVisible);
+  };
+  const showDroplist = (setter) => {
+    setter(true);
+  };
+  const hideDroplist = (setter) => {
+    setter(false);
+  };
+
   return (
-    <nav id="sidebar">
+    <nav id="sidebar" className={sidebarVisible ? "" : "close"}>
       <ul>
         <li>
           <span className="main-logo">
@@ -29,20 +43,20 @@ function Sidebar() {
               </defs>
             </svg>
 
-            <button id="toggle-btn">
+            <button id="toggle-btn" onClick={() => toggleSidebar()}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                height="24px"
+                height="30px"
                 viewBox="0 -960 960 960"
-                width="24px"
+                width="30px"
               >
                 <path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z" />
               </svg>
             </button>
           </span>
         </li>
-        {/* <li className="active"> */}
-        <li>
+        <li className="active">
+          {/* <li> */}
           <Link to="/tools/dashboard">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -52,10 +66,13 @@ function Sidebar() {
             >
               <path d="M520-600v-240h320v240H520ZM120-440v-400h320v400H120Zm400 320v-400h320v400H520Zm-400 0v-240h320v240H120Zm80-400h160v-240H200v240Zm400 320h160v-240H600v240Zm0-480h160v-80H600v80ZM200-200h160v-80H200v80Zm160-320Zm240-160Zm0 240ZM360-280Z" />
             </svg>
-            Dashboard
+            <span>Dashboard</span>
           </Link>
         </li>
-        <li className="active">
+        <li
+          className="active dropdown-btn"
+          onMouseLeave={() => hideDroplist(setCalenderVisible)}
+        >
           <Link to="/tools/admission-calender">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -65,7 +82,45 @@ function Sidebar() {
             >
               <path d="M240-280h240v-80H240v80Zm120-160h240v-80H360v80Zm120-160h240v-80H480v80ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm0-560v560-560Z" />
             </svg>
-            Admission Calender
+            <span>Admission</span>
+            <div
+              className="dropdown"
+              onMouseEnter={() => showDroplist(setCalenderVisible)}
+            >
+              <svg
+                className={calenderVisible ? "drop-icon rotated" : "drop-icon"}
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+              >
+                <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+              </svg>
+            </div>
+          </Link>
+          <ul className={calenderVisible ? "sub-menu show" : "sub-menu"}>
+            <div>
+              <li>
+                <Link to="/tools/admission-calender">Timeline</Link>
+              </li>
+              <li>
+                <Link to="/tools/dashboard">Finder</Link>
+              </li>
+            </div>
+          </ul>
+        </li>
+        <li className="">
+          {/* <li> */}
+          <Link to="/tools/dashboard">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+            >
+              <path d="M520-600v-240h320v240H520ZM120-440v-400h320v400H120Zm400 320v-400h320v400H520Zm-400 0v-240h320v240H120Zm80-400h160v-240H200v240Zm400 320h160v-240H600v240Zm0-480h160v-80H600v80ZM200-200h160v-80H200v80Zm160-320Zm240-160Zm0 240ZM360-280Z" />
+            </svg>
+            <span>Dashboard</span>
           </Link>
         </li>
       </ul>
